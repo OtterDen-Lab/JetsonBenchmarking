eb1e7aac02407503ee3814b3eebf1c237c7e3c92
Sam O <2274654+samogden@users.noreply.github.com>
2023-11-20 16:55:08 -0800

    Adding in MWE

--- src/Scripts/MNIST_ssogden.py
+++ src/Scripts/MNIST_ssogden.py
@@ -54,22 +54,9 @@ def main():
         as_supervised=True,
         with_info=True,
     )
-    def normalize_img(image, label):
-        """Normalizes images: `uint8` -> `float32`."""
-        return tf.cast(image, tf.float32) / 255., label
     
-    # ds_train = ds_train.map(
-    #     normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    # ds_train = ds_train.cache()
-    # ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
     ds_train = ds_train.batch(128)
-    # ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
-    
-    ds_test = ds_test.map(
-        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_test = ds_test.batch(128)
-    ds_test = ds_test.cache()
-    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
     
     #creates the NNM
     model = tf.keras.models.Sequential([
